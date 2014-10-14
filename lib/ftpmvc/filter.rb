ff270bef5f4d320885ae811f88f38223b6ec7ad6
André Aizim Kelmanson <akelmanson@gmail.com>
2014-10-14 15:56:39 -0300

    adding default methods to Filter

--- lib/ftpmvc/filter.rb
+++ lib/ftpmvc/filter.rb
@@ -1,11 +1,11 @@
 module FTPMVC
   class Filter
+    extend Forwardable
+
+    def_delegators :@chain, :index, :size, :get, :directory?
+
     def initialize(fs, chain)
       @fs, @chain = fs, chain
-    end
-
-    def index(path)
-      @chain.index(path)
     end
   end
 end
